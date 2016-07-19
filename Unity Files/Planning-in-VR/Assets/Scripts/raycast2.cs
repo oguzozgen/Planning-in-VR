f686ccfdfc4bd32bd6cbb347fb6670d523011baf
Oguz Ozgen <oguzozgen@outlook.com>
2016-07-19 15:24:24 +0300

    raycast + vertex based web browser
    raycast + vertex based web browser

---
+++ Unity Files/Planning-in-VR/Assets/Scripts/raycast2.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class raycast2 : MonoBehaviour {
+
+    void Update()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+
+        if (Input.GetKey(KeyCode.Space))
+        {
+             if (Physics.Raycast(ray, 100))
+             {
+                 GameObject g = hit.collider.gameObject; string name = g.name; 
+            print("Hit something!" +name);
+            
+        }
+
+        if (Physics.Raycast(ray, out hit, 100))
+            Debug.DrawLine(ray.origin, hit.point); 
+        } 
+       
+      
+    }
+}
